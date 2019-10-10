75926204927bd3409be76bbad303d0f7e92f6002
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-10-10 10:55:14 +0000

    create a new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,3 +2,6 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: {minimun: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
+
+
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
 root 'pages#home'
 get 'about', to: 'pages#about'
+resources :articles 
 
 end
