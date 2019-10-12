b44dc5b79704a24d9a4490f9e3e0dcf56f5437b4
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-10-12 08:43:00 +0000

    Add edit update functionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ def new
 end
 
 def create
-  debugger
 @user = User.new(user_params)
 if @user.save
 flash[:success] = "Welcome to the alpha blog #{@user.username}"
@@ -15,6 +14,19 @@ else
 end
 end
 
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+@user = User.find(params[:id])
+if @user.update(user_params)
+flash[:success] = "Your account was updated successfully"
+redirect_to articles_path
+else
+  render 'edit'
+end
+end
 
 private
 def user_params
