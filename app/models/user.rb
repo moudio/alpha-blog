26dd4569ba62acc9afd477ae2a1b3efc737d8465
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-10-12 08:01:00 +0000

    Add secure password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence: true, length: {maximum: 105},
 uniqueness: {case_sensitive: false},
 format: { with: VALID_EMAIL_REGEX }
+has_secure_password
 end
