cd16d52df0c6ae069f8cdf93b23d3a78f7e57f6d
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-10-12 10:58:12 +0000

    Add pagination to views

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby '2.6.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 gem 'bcrypt', '~> 3.1.7'
+gem 'will_paginate', '3.1.7'
+gem 'bootstrap-will_paginate', '1.0.0'
 
 # Use sqlite3 as the database for Active Record
 # Use Puma as the app server
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
 def new
@@ -33,6 +33,7 @@ end
 
 def show
   @user = User.find(params[:id])
+@user_articles = @user.articles.paginate(page: params[:page], per_page: 4)
 end
 
 private
